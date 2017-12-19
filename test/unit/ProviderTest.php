4c32a35f11aeb1f7cf5af0e62183a60f60a5e4f5
sc0Vu <alk03073135@gmail.com>
2017-12-19 11:58:49 +0800

    ProviderTest

--- test/unit/ProviderTest.php
+++ test/unit/ProviderTest.php
@@ -3,11 +3,26 @@
 namespace Test\Unit;
 
 use Test\TestCase;
+use Web3\RequestManagers\RequestManager;
+use Web3\Providers\Provider;
 
 class ProviderTest extends TestCase
 {
-    public function testHelloWorld()
+    /**
+     * testSetRequestManager
+     * 
+     * @return void
+     */
+    public function testSetRequestManager()
     {
-        $this->assertTrue(true);
+        $requestManager = new RequestManager('http://localhost:8545');
+        $provider = new Provider($requestManager);
+
+        $this->assertEquals($provider->requestManager->host, 'http://localhost:8545');
+
+        $requestManager = new RequestManager($this->testRinkebyHost);
+        $provider->requestManager = $requestManager;
+
+        $this->assertEquals($provider->requestManager->host, 'http://localhost:8545');
     }
 }
