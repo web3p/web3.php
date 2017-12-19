303c14a842974e673d8717d14a9d060083803c68
sc0Vu <alk03073135@gmail.com>
2017-12-19 16:15:17 +0800

    PersonalBatchTest

---
+++ test/unit/PersonalBatchTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Test\Unit;
+
+use RuntimeException;
+use Test\TestCase;
+
+class PersonalBatchTest extends TestCase
+{
+    /**
+     * personal
+     * 
+     * @var Web3\Personal
+     */
+    protected $personal;
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->personal = $this->web3->personal;
+    }
+
+    /**
+     * testBatch
+     * 
+     * @return void
+     */
+    public function testBatch()
+    {
+        $personal = $this->personal;
+
+        $personal->batch(true);
+        $personal->listAccounts();
+        $personal->newAccount('123456');
+
+        $personal->provider->execute(function ($err, $data) {
+            if ($err !== null) {
+                return $this->assertTrue($err->getCode() === 405);
+            }
+            $this->assertTrue(is_array($data[0]->result));
+            $this->assertTrue(is_string($data[1]->result));
+        });
+    }
+}
