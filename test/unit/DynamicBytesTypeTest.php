d957484fdc41ae551bf25913fd10b23c559b141e
sc0Vu <alk03073135@gmail.com>
2017-12-26 17:42:10 +0800

    DynamicBytesType

---
+++ test/unit/DynamicBytesTypeTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Test\Unit;
+
+use InvalidArgumentException;
+use Test\TestCase;
+use Web3\Contracts\Types\DynamicBytes;
+
+class DynamicBytesTypeTest extends TestCase
+{
+    /**
+     * testTypes
+     * 
+     * @var array
+     */
+    protected $testTypes = [
+        [
+            'value' => 'bytes',
+            'result' => true
+        ], [
+            'value' => 'bytes[]',
+            'result' => true
+        ], [
+            'value' => 'bytes[4]',
+            'result' => true
+        ], [
+            'value' => 'bytes[][]',
+            'result' => true
+        ], [
+            'value' => 'bytes[3][]',
+            'result' => true
+        ], [
+            'value' => 'bytes[][6][]',
+            'result' => true
+        ], [
+            'value' => 'bytes32',
+            'result' => true
+        ], [
+            'value' => 'bytes8[4]',
+            'result' => true
+        ],
+    ];
+
+    /**
+     * solidityType
+     * 
+     * @var \Web3\Contracts\SolidityType
+     */
+    protected $solidityType;
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->solidityType = new DynamicBytes;
+    }
+
+    /**
+     * testIsType
+     * 
+     * @return void
+     */
+    public function testIsType()
+    {
+        $solidityType = $this->solidityType;
+
+        foreach ($this->testTypes as $type) {
+            $this->assertEquals($solidityType->isType($type['value']), $type['result']);
+        }
+    }
+}
