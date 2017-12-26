0fa64aecd74c3687e8786b321fbe13c31ce0f368
sc0Vu <alk03073135@gmail.com>
2017-12-26 16:46:52 +0800

    AddressType

---
+++ src/Contracts/SolidityType.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of web3.php package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace Web3\Contracts;
+
+class SolidityType
+{
+    /**
+     * construct
+     * 
+     * @return void
+     */
+    public function  __construct()
+    {
+        // 
+    }
+
+    /**
+     * get
+     * 
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $method = 'get' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], []);
+        }
+    }
+
+    /**
+     * set
+     * 
+     * @param string $name
+     * @param mixed $value
+     * @return bool
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], [$value]);
+        }
+        return false;
+    }
+}
