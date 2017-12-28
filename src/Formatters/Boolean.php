c6c5cc9251256375164cbec85bf0b1f5cfb3cab7
sc0Vu <alk03073135@gmail.com>
2017-12-28 11:04:56 +0800

    Boolean formatter.

---
+++ src/Formatters/Boolean.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of web3.php package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace Web3\Formatters;
+
+use InvalidArgumentException;
+use Web3\Utils;
+use Web3\Formatters\IFormatter;
+
+class Boolean implements IFormatter
+{
+    /**
+     * format
+     * 
+     * @param mixed $value
+     * @return int
+     */
+    public static function format($value)
+    {
+        if (!is_bool($value)) {
+            throw new InvalidArgumentException('The value to inputFormat function must be boolean.');
+        }
+        return (int) $value;
+    }
+}
---
+++ test/unit/BooleanFormatterTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use Web3\Formatters\Boolean;
+
+class BooleanFormatterTest extends TestCase
+{
+    /**
+     * formatter
+     * 
+     * @var \Web3\Formatters\Boolean
+     */
+    protected $formatter;
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->formatter = new Boolean;
+    }
+
+    /**
+     * testFormat
+     * 
+     * @return void
+     */
+    public function testFormat()
+    {
+        $formatter = $this->formatter;
+
+        $boolean = $formatter->format(true);
+
+        $this->assertEquals($boolean, 1);
+
+        $boolean = $formatter->format(false);
+
+        $this->assertEquals($boolean, 0);
+    }
+}
