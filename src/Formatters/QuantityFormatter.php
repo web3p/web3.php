e130d4506856fed364b574bd22969d8bf4f1af4b
sc0Vu <alk03073135@gmail.com>
2018-01-24 12:25:01 +0800

    Quantity formatter test.

--- src/Formatters/QuantityFormatter.php
+++ src/Formatters/QuantityFormatter.php
@@ -26,8 +26,22 @@ class QuantityFormatter implements IFormatter
     public static function format($value)
     {
         $value = Utils::toString($value);
-        $bn = Utils::toBn($value);
 
-        return '0x' . $bn->toHex(true);
+        if (Utils::isZeroPrefixed($value)) {
+            // test hex with zero ahead, hardcode 0x0
+            if ($value === '0x0' || strpos($value, '0x0') !== 0) {
+                return $value;
+            }
+            $hex = preg_replace('/^0x0+(?!$)/', '', $value);
+        } else {
+            $bn = Utils::toBn($value);
+            $hex = $bn->toHex(true);        
+        }
+        if (empty($hex)) {
+            $hex = '0';
+        } else {
+            $hex = preg_replace('/^0+(?!$)/', '', $hex);
+        }
+        return '0x' . $hex;
     }
 }
