5e12df17a55efec397f42bc3cefe4ab75cc6d095
sc0Vu <alk03073135@gmail.com>
2018-01-26 16:20:51 +0800

    shh_version

---
+++ src/Methods/Shh/Version.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of web3.php package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace Web3\Methods\Shh;
+
+use InvalidArgumentException;
+use Web3\Methods\EthMethod;
+
+class Version extends EthMethod
+{
+    /**
+     * validators
+     * 
+     * @var array
+     */
+    protected $validators = [];
+
+    /**
+     * inputFormatters
+     * 
+     * @var array
+     */
+    protected $inputFormatters = [];
+
+    /**
+     * outputFormatters
+     * 
+     * @var array
+     */
+    protected $outputFormatters = [];
+
+    /**
+     * defaultValues
+     * 
+     * @var array
+     */
+    protected $defaultValues = [];
+
+    /**
+     * construct
+     * 
+     * @param string $method
+     * @param array $arguments
+     * @return void
+     */
+    // public function __construct($method='', $arguments=[])
+    // {
+    //     parent::__construct($method, $arguments);
+    // }
+}
