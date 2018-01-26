5d54bc29c3287c6143acc4b7d018f54d496a5dbe
sc0Vu <alk03073135@gmail.com>
2018-01-26 17:15:36 +0800

    Identity validator.

---
+++ src/Validators/IdentityValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of web3.php package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace Web3\Validators;
+
+use Web3\Validators\IValidator;
+
+class IdentityValidator
+{
+    /**
+     * validate
+     *
+     * @param string $value
+     * @return bool
+     */
+    public static function validate($value)
+    {
+        if (!is_string($value)) {
+            return false;
+        }
+        return (preg_match('/^0x[a-fA-F0-9]+$/', $value) >= 1);
+    }
+}
