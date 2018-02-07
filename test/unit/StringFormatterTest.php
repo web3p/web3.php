f528660abb6b69eaf2bb29ae7254d50b39715f43
sc0Vu <alk03073135@gmail.com>
2018-02-07 17:33:45 +0800

    Add StringFormatter test.

---
+++ test/unit/StringFormatterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use Web3\Formatters\StringFormatter;
+
+class StringFormatterTest extends TestCase
+{
+    /**
+     * formatter
+     * 
+     * @var \Web3\Formatters\StringFormatter
+     */
+    protected $formatter;
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->formatter = new StringFormatter;
+    }
+
+    /**
+     * testFormat
+     * 
+     * @return void
+     */
+    public function testFormat()
+    {
+        $formatter = $this->formatter;
+
+        $str = $formatter->format(123456);
+        $this->assertEquals($str, '123456');
+    }
+}
