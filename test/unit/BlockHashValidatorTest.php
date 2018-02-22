15459d67df539f954801310a58d5bf3030ac4ce7
sc0Vu <alk03073135@gmail.com>
2018-02-22 14:26:15 +0800

    Block hash validator test.

---
+++ test/unit/BlockHashValidatorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use Web3\Validators\BlockHashValidator;
+
+class BlockHashValidatorTest extends TestCase
+{
+    /**
+     * validator
+     * 
+     * @var \Web3\Validators\BlockHashValidator
+     */
+    protected $validator;
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->validator = new BlockHashValidator;
+    }
+
+    /**
+     * testValidate
+     * 
+     * @return void
+     */
+    public function testValidate()
+    {
+        $validator = $this->validator;
+
+        $this->assertEquals(false, $validator->validate('0Xca35b7d915458ef540ade6068dfe2f44e8fa733c'));
+        $this->assertEquals(false, $validator->validate('0XCA35B7D915458EF540ADE6068DFE2F44E8FA733C'));
+        $this->assertEquals(false, $validator->validate('0xcA35b7D915458eF540ade6068Dfe2f44e8fA733ccA35b7D915458eF540ade6068Dfe2f44e8fA733c'));
+        $this->assertEquals(false, $validator->validate('CA35B7D915458EF540ADE6068DFE2F44E8FA733C'));
+        $this->assertEquals(false, $validator->validate('1234'));
+        $this->assertEquals(false, $validator->validate('abcd'));
+        $this->assertEquals(false, $validator->validate(0xCA35B7D915458EF540ADE6068DFE2F44E8FA733C));
+        $this->assertEquals(true, $validator->validate('0xEB0B54D62EC3F561C2EEBDAEBD92432126F0817579C102B062D1A6C1F2ED83E8'));
+        $this->assertEquals(true, $validator->validate('0xeb0b54d62ec3f561c2eebdaebd92432126f0817579c102b062d1a6c1f2ed83e8'));
+        $this->assertEquals(true, $validator->validate('0xeb0b54D62ec3f561C2eebdaebd92432126F0817579c102b062d1a6c1f2ed83e8'));
+    }
+}
