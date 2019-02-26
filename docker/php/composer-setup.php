10b000d3e55efc77b3bdfe965885104e15632e8e
sc0vu <alk03073135@gmail.com>
2019-02-26 18:50:22 +0800

    Update docker/php/composer-setup.php

--- docker/php/composer-setup.php
+++ docker/php/composer-setup.php
@@ -1,5 +1,8 @@
 <?php
 
+$sig = file_get_contents('https://composer.github.io/installer.sig');
+$sig = trim($sig);
+
 copy('https://getcomposer.org/installer', 'composer-setup.php');
 
-if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;
+if (hash_file('sha384', 'composer-setup.php') === $sig) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;
