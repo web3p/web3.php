d0289506dc0331b751cf6754d3afca304ff71011
sc0vu <alk03073135@gmail.com>
2019-10-03 00:01:35 +0800

    Add toChecksumAddress in Utils.php
    See #150 (https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md)

--- test/unit/UtilsTest.php
+++ test/unit/UtilsTest.php
@@ -249,7 +249,7 @@ class UtilsTest extends TestCase
 
     /**
      * testIsAddressChecksum
-     * 
+     *
      * @return void
      */
     public function testIsAddressChecksum()
@@ -292,8 +292,35 @@ class UtilsTest extends TestCase
     }
 
     /**
+     * testToChecksumAddress
+     *
+     * @return void
+     */
+    public function testToChecksumAddress()
+    {
+        $checksumAddressTest = [
+            // All caps
+            '0x52908400098527886E0F7030069857D2E4169EE7',
+            '0x8617E340B3D01FA5F11F306F4090FD50E238070D',
+            // All Lower
+            '0xde709f2102306220921060314715629080e2fb77',
+            '0x27b1fdb04752bbc536007a920d24acb045561c26',
+            // Normal
+            '0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed',
+            '0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359',
+            '0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB',
+            '0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb'
+        ];
+
+        for ($i=0; $i<count($checksumAddressTest); $i++) {
+            $checksumAddress = Utils::toChecksumAddress(strtolower($checksumAddressTest[$i]));
+            $this->assertEquals($checksumAddressTest[$i], $checksumAddress);
+        }
+    }
+
+    /**
      * testStripZero
-     * 
+     *
      * @return void
      */
     public function testStripZero()
